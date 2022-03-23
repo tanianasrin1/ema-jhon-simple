4a766ad81630af40b395e3f6256b1a3b5033b918
tanianasrin1 <tasrineva169@gmail.com>
2022-03-23 11:08:50 +0600

    added cart calculated components

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,10 +1,24 @@
 import React from 'react';
+import './Cart.css'
 
-const Cart = ({cart}) => {
+const Cart = (props) => {
+    const {cart} = props
+    // console.log(cart)
+    let total = 0
+    let shipping = 0
+    for(const product of cart){
+        total = total + product.price
+        shipping = shipping + product.shipping
+    }
+    const tax = (total*0.1).toFixed(2)
     return (
-        <div>
+        <div className='cart'>
              <h3>Oder summary</h3>
              <p>selected item: {cart.length} </p>
+             <p>Total price: ${total} </p>
+             <p>Total shopping charge: ${shipping}  </p>
+             <p>Tax: ${tax}  </p>
+             <h2>Grand Total:  </h2>
         </div>
     );
 };
