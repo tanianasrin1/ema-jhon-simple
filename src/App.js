b2f667d6bf83b128afe68ef1697f572c40e5b385
tanianasrin1 <tasrineva169@gmail.com>
2022-04-12 13:49:07 +0600

    ema john sign up

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import Inventory from './components/Inventory/Inventory';
 import Login from './components/Login/Login';
 import Order from './components/Order/Order';
 import Shop from './components/Shop/Shop';
+import SignUp from './components/SignUp/SignUp';
 
 
 function App() {
@@ -20,6 +21,7 @@ function App() {
         <Route path='/inventory' element= {<Inventory></Inventory>} ></Route>
         <Route path='/about' element= {<About></About>} ></Route>
         <Route path='/login' element= {<Login></Login>}></Route>
+        <Route path='/signUp' element= {<SignUp></SignUp>}></Route>
       </Routes>
   
     
--- src/components/SignUp/SignUp.js
+++ src/components/SignUp/SignUp.js
@@ -1,10 +1,60 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+
 
 const SignUp = () => {
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const [confirmPassword, setConfirmPassword] = useState('');
+    const [error, setError] = useState('');
+
+    const handleEmailBlur = event =>{
+        setEmail(event.target.value)
+    }
+
+    const handlePasswordBlur = event =>{
+        setPassword(event.target.value)
+    }
+
+    const handleConfirmPasswordBlur = event =>{
+        setConfirmPassword(event.target.value)
+    }
+   
+    const handleCreateUser = event =>{
+        event.preventDefault()
+        if(password !== confirmPassword){
+            setError('you two password not match')
+            return;
+        }
+    }
+    
+
     return (
+        <div className="form-container">
         <div>
-            <h2>This is signUp page</h2>
+          <h2 className="form-title">SignUP</h2>
+          <form>
+             <div className="input-group">
+                 <label htmlFor="email"> Email</label>
+                 <input onBlur={handleEmailBlur} type="email" name="email" id="" required />
+  
+             </div>
+             <div className="input-group">
+                 <label htmlFor="password">Password</label>
+                 <input onBlur={handlePasswordBlur} type="password" name="password" id="" required />
+  
+             </div>
+             <div className="input-group">
+                 <label htmlFor="Confirm-password">Confirm-Password</label>
+                 <input onBlur={handleConfirmPasswordBlur} type="password" name="Confirm-password" id="" required />
+  
+             </div>
+             <p style= {{color:'red'}}>{error}</p>
+             <input className="form-submit" type="submit" value="Sign UP" />
+          </form>
+          <p>Already have a account? <Link className="form-link" to='/signUp'>Login</Link> </p>
         </div>
+      </div>
     );
 };
 
